349da49a636b0c8dbd7017e5e93fee507287e164
mytime <cmba@qq.com>
2016-05-24 14:33:51 +0800

    Mysql

---
+++ studentinfo.sql
@@ -0,0 +1,57 @@
+/*
+SQLyog Trial v11.4 (32 bit)
+MySQL - 10.1.13-MariaDB : Database - jkxystudent
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`jkxystudent` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+
+USE `jkxystudent`;
+
+/*Table structure for table `studentinfo` */
+
+DROP TABLE IF EXISTS `studentinfo`;
+
+CREATE TABLE `studentinfo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nicheng` char(10) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `truename` char(10) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `xb` bit(1) DEFAULT NULL,
+  `csrq` date DEFAULT NULL,
+  `zy` char(10) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `kc` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `xq` char(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `bz` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `studentinfo` */
+
+insert  into `studentinfo`(`id`,`nicheng`,`truename`,`xb`,`csrq`,`zy`,`kc`,`xq`,`bz`) values (2,'福娃','Jason','','1982-02-26','通信工程','数据库原理','web','我爱你'),(3,'张四','张大勇','','1982-02-28','通信工程','数据结构','music&web','我很棒'),(4,'福','闫万福','','1982-02-24','计算机科学与技术','数据库原理','music','我很帅哦');
+
+/*Table structure for table `usertable` */
+
+DROP TABLE IF EXISTS `usertable`;
+
+CREATE TABLE `usertable` (
+  `username` char(10) COLLATE utf8_unicode_ci NOT NULL,
+  `password` char(10) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `usertable` */
+
+insert  into `usertable`(`username`,`password`) values ('11','11'),('22','22');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
